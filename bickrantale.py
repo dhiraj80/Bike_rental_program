67cb8646ea3938460591ceb8bcdf0a007ef005e5
Dhiraj Arya <91623511+dhiraj80@users.noreply.github.com>
2021-11-27 19:40:07 +0530

    Add files via upload

---
+++ bickrantale.py
@@ -0,0 +1,14 @@
+# Auther = Dhiraj Arya
+# Github username = dhiraj80
+# Email = dhirajkum00380@gmail.com
+print('welcome to bike rantale program')
+totalbike = 100
+bikerent = 1500
+print("Total Bike For Rent Availale(pic.) --> ",totalbike )
+print("Bike Rent Price --> ",bikerent)
+rentqty = int(input("Plaese Enter Quntity for Bike on Rent --> "))
+availalebike = totalbike-rentqty
+rentprice = bikerent*rentqty
+
+print("Your Total Rent For Paymeant --> ",rentprice)
+print("Total Availale Bike For Rent(pic.)",availalebike)
