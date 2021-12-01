a76deb9d3f7658fdccbd80cf8b7942129ec5fc34
Dhiraj Arya <91623511+dhiraj80@users.noreply.github.com>
2021-12-01 20:27:10 +0530

    oops concepts use

--- bickrantale.py
+++ bickrantale.py
@@ -1,14 +1,40 @@
 # Auther = Dhiraj Arya
 # Github username = dhiraj80
 # Email = dhirajkum00380@gmail.com
-print('welcome to bike rantale program')
-totalbike = 100
-bikerent = 1500
-print("Total Bike For Rent Availale(pic.) --> ",totalbike )
-print("Bike Rent Price --> ",bikerent)
-rentqty = int(input("Plaese Enter Quntity for Bike on Rent --> "))
-availalebike = totalbike-rentqty
-rentprice = bikerent*rentqty
 
-print("Your Total Rent For Paymeant --> ",rentprice)
-print("Total Availale Bike For Rent(pic.)",availalebike)
+class BikeRent:
+    def __init__(self,stock):
+        self.stock = stock
+        print('''
+        1.Availale stock is -> 
+        2.Rent a bike ->
+        3.Exit
+        ''')
+    def stocks(self):
+        print('Availale stock is -->',self.stock)
+
+    def qty(self,q):
+        if q<=0:
+            print("Enter a correct value")
+        elif q>self.stock:
+            print("Enter a correct value(lessden qty)")
+        else:
+            print(f'Total bill is -> {q*500}')
+            print('Availale stock is -> ',self.stock-q )
+        
+
+obj = BikeRent(100)
+
+while True:
+    user = int(input('Enter a value-> '))
+
+    if user ==1:
+        obj.stocks()
+        print("")
+
+    elif user ==2:
+        qty = int(input('Enter a Qty (less then stock)-> '))
+        obj.qty(qty)
+        print("")
+    else:
+        break
